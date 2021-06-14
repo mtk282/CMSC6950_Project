b289af83dd49f70c0149a0d7dc03ee5cf2fc0863
Michael King <mtk282@mun.ca>
2021-06-14 08:28:39 -0230

    Same colour scale used for all 6 plots

--- plot_total_argo.py
+++ plot_total_argo.py
@@ -55,17 +55,17 @@ def main():
 
     # Plot total number of argo floats for each year
 
-    df_2001.plot(ax=ax[0,0], x="LONGITUDE", y="LATITUDE", kind="scatter", c="year", colormap="viridis", s=1)
+    df_2001.plot(ax=ax[0,0], x="LONGITUDE", y="LATITUDE", kind="scatter", c="year", colormap="viridis", vmin =2001, vmax=2006, s=1)
     ax[0, 0].set_title(f'Argo Floats in 2001 (Total # of Floats = {len(df_2001)})')
-    d1.plot(ax=ax[1,0], x="LONGITUDE", y="LATITUDE", kind="scatter", c="year", colormap="viridis", s=1)
+    d1.plot(ax=ax[1,0], x="LONGITUDE", y="LATITUDE", kind="scatter", c="year", colormap="viridis", vmin =2001, vmax =2006, s=1)
     ax[1, 0].set_title(f'Argo Floats in 2002 (Total # of Floats = {len(d1)})')
-    d2.plot(ax=ax[2,0], x="LONGITUDE", y="LATITUDE", kind="scatter", c="year", colormap="viridis", s=1)
+    d2.plot(ax=ax[2,0], x="LONGITUDE", y="LATITUDE", kind="scatter", c="year", colormap="viridis", vmin =2001, vmax =2006, s=1)
     ax[2, 0].set_title(f'Argo Floats in 2003 (Total # of Floats = {len(d2)})')
-    d3.plot(ax=ax[0,1], x="LONGITUDE", y="LATITUDE", kind="scatter", c="year", colormap="viridis", s=1)
+    d3.plot(ax=ax[0,1], x="LONGITUDE", y="LATITUDE", kind="scatter", c="year", colormap="viridis", vmin =2001, vmax =2006, s=1)
     ax[0, 1].set_title(f'Argo Floats in 2004 (Total # of Floats = {len(d3)})')
-    d4.plot(ax=ax[1,1], x="LONGITUDE", y="LATITUDE", kind="scatter", c="year", colormap="viridis", s=1)
+    d4.plot(ax=ax[1,1], x="LONGITUDE", y="LATITUDE", kind="scatter", c="year", colormap="viridis", vmin =2001, vmax = 2006, s=1)
     ax[1, 1].set_title(f'Argo Floats in 2005 (Total # of Floats = {len(d4)})')
-    d5.plot(ax=ax[2,1], x="LONGITUDE", y="LATITUDE", kind="scatter", c="year", colormap="viridis", s=1)
+    d5.plot(ax=ax[2,1], x="LONGITUDE", y="LATITUDE", kind="scatter", c="year", colormap="viridis", vmin =2001, vmax=2006, s=1)
     ax[2, 1].set_title(f'Argo Floats in 2006 (Total # of Floats = {len(d5)})')
 
 
