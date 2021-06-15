2b4d2ee48b037f44fce4eb2865e0bce59a83a866
Michael King <mtk282@mun.ca>
2021-06-15 11:00:11 -0230

    Comment changes

--- argo_float_sort.py
+++ argo_float_sort.py
@@ -1,13 +1,11 @@
-##Task 2 - Seperate 2 Argo floats into two dataframes and export as csv files
+##Task 2 - Import argopy_task2.csv and seperate 2 Argo floats into two dataframes and them export as csv files
 
 #Import python libraries
 
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import geopandas as gpd
 
-#Import Argo float data from a csv file as data frame
+#Import Argo float data from a csv file as dataframe
 
 df = pd.read_csv('argopy_task2.csv', parse_dates=[15]) ##Load in csv file and identify date columns
 
@@ -18,5 +16,5 @@ df_6902696 = df[df.PLATFORM_NUMBER == 6902696]
 
 #Export Argo dataframes to csv files
 
-df_6902754.to_csv (r'argo_6902754.csv', index = False, header=True)  ##Export data to a csv file
-df_6902696.to_csv (r'argo_6902696.csv', index = False, header=True)  ##Export data to a csv file
+df_6902754.to_csv (r'argo_6902754.csv', index = False, header=True)
+df_6902696.to_csv (r'argo_6902696.csv', index = False, header=True)
